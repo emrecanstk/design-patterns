67de031d020216efcb29b6e364d8d6bf08652095
Emre Can Satık <emrecanstk@gmail.com>
2022-07-01 22:05:35 +0300

    Add information about patterns

--- Creational Design Patterns/Singleton/BillPugh.java
+++ Creational Design Patterns/Singleton/BillPugh.java
@@ -12,4 +12,8 @@ public class BillPugh {
         return SingletonHelper.instance;
     }
     
+    // Notice the private inner static class that contains the instance of the singleton class.
+    // When the singleton class is loaded, SingletonHelper class is not loaded into memory and only when someone calls the getInstance method, this class gets loaded and creates the Singleton class instance.
+    // This is the most widely used approach for Singleton class as it doesn’t require synchronization.
+
 }
--- Creational Design Patterns/Singleton/Enum.java
+++ Creational Design Patterns/Singleton/Enum.java
@@ -5,4 +5,8 @@ public enum Enum {
     public static void doSomething() {
         // do something
     }
+
+    // Since Java Enum values are globally accessible, so is the singleton.
+    // The drawback is that the enum type is somewhat inflexible; for example, it does not allow lazy initialization.
+
 }
