53e26e6f832099037a4f17665c8555b8f5215522
Emre Can Satık <emrecanstk@gmail.com>
2022-07-01 22:33:07 +0300

    Add information about factory design

--- Creational Design Patterns/Factory/ComputerFactory.java
+++ Creational Design Patterns/Factory/ComputerFactory.java
@@ -6,3 +6,5 @@ public class ComputerFactory {
 
 // The method takes one Class type parameter.
 // This parameter is to understand which class we want to create, but the factory class does not know which class it created.
+
+// It knows that there is a class that derives from the Computer interface, whose return type is Computer.
