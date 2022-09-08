5ee9807d76b3fcad451062c3c4cfeabccf7a2477
emrecanstk <emrecanstk@gmail.com>
2022-09-08 15:32:04 +0300

    Create PrototypePatternTest.java

---
+++ Creational Design Patterns/Prototype/PrototypePatternTest.java
@@ -0,0 +1,27 @@
+/*
+ * Notice that the clone method is overridden to provide a deep copy of the employees list.
+ * Here is the prototype design pattern example test program that will show the benefit of prototype pattern.
+ */
+
+import java.util.List;
+
+public class PrototypePatternTest {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+		Employees emps = new Employees();
+		emps.loadData();
+		
+		//Use the clone method to get the Employee object
+		Employees empsNew = (Employees) emps.clone();
+		Employees empsNew1 = (Employees) emps.clone();
+		List<String> list = empsNew.getEmpList();
+		list.add("John");
+		List<String> list1 = empsNew1.getEmpList();
+		list1.remove("Pankaj");
+		
+		System.out.println("emps List: "+emps.getEmpList());
+		System.out.println("empsNew List: "+list);
+		System.out.println("empsNew1 List: "+list1);
+	}
+
+}
