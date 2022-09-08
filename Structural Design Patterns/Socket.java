9b008c23cb7c6ebea8edc0a08edabd0eeafa1ee0
emrecanstk <emrecanstk@gmail.com>
2022-09-08 16:16:14 +0300

    Create Socket.java SocketAdapter.java Volt.java

---
+++ Structural Design Patterns/Socket.java
@@ -0,0 +1,6 @@
+public class Socket {
+
+	public Volt getVolt(){
+		return new Volt(120);
+	}
+}
---
+++ Structural Design Patterns/SocketAdapter.java
@@ -0,0 +1,8 @@
+public interface SocketAdapter {
+
+	public Volt get120Volt();
+		
+	public Volt get12Volt();
+	
+	public Volt get3Volt();
+}
